94b98d5d882dc10466326630756c77afe70e050d
Cristian <llcristianaceroll@gmail.com>
2021-06-25 16:02:07 -0500

    bucles

---
+++ bucles.py
@@ -0,0 +1,7 @@
+# contador = 0
+# print('2 elevador a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 1
+# print('2 elevador a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+
