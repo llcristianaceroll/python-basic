828615cbd32d9068833365473fef17d1da4f135d
Cristian <llcristianaceroll@gmail.com>
2021-06-25 10:07:14 -0500

    funciones - mejorando el conversor

--- conversor.py
+++ conversor.py
@@ -1,3 +1,11 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input('Cuantos pesos' + tipo_pesos + ' tienes?: ')
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
 menu = """
 Bienvenido al conversor de monedas ✨ 
 
@@ -10,30 +18,53 @@ Elige una opcion: """
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = input('Cuantos pesos Colombianos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor('Colombianos', 3875)
 elif opcion == 2:
-    pesos = input('Cuantos pesos Argentinos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor('Argentinos', 65)   
 elif opcion == 3:
-    pesos = input('Cuantos pesos Mexicanos tienes?: ')
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor('Mexicanos', 24)    
 else:
     print('Ingresa un opcion correcta')
+
+
+
+
+# menu = """
+# Bienvenido al conversor de monedas ✨ 
+
+# 1. Pesos Colombianos
+# 2. Pesos argentinos
+# 3. Pesos Mexicanos
+
+# Elige una opcion: """
+
+# opcion = int(input(menu))
+
+# if opcion == 1:
+#     pesos = input('Cuantos pesos Colombianos tienes?: ')
+#     pesos = float(pesos)
+#     valor_dolar = 3875
+#     dolares = pesos / valor_dolar
+#     dolares = round(dolares, 2)
+#     dolares = str(dolares)
+#     print("Tienes $" + dolares + " dolares")
+# elif opcion == 2:
+#     pesos = input('Cuantos pesos Argentinos tienes?: ')
+#     pesos = float(pesos)
+#     valor_dolar = 65
+#     dolares = pesos / valor_dolar
+#     dolares = round(dolares, 2)
+#     dolares = str(dolares)
+#     print("Tienes $" + dolares + " dolares")
+# elif opcion == 3:
+#     pesos = input('Cuantos pesos Mexicanos tienes?: ')
+#     pesos = float(pesos)
+#     valor_dolar = 24
+#     dolares = pesos / valor_dolar
+#     dolares = round(dolares, 2)
+#     dolares = str(dolares)
+#     print("Tienes $" + dolares + " dolares")
+# else:
+#     print('Ingresa un opcion correcta')
 
 
--- funciones.py
+++ funciones.py
@@ -7,20 +7,27 @@
 # imprimir_mensaje()
 
 
-def conversacion(mensaje):
-    print('Hola')
-    print('Como estas?')
-    print(mensaje)
-    print('Adios')
+# def conversacion(mensaje):
+#     print('Hola')
+#     print('Como estas?')
+#     print(mensaje)
+#     print('Adios')
 
 
-opcion = int(input('Elige una opcion (1, 2, 3): '))
-if opcion == 1:
-    conversacion('Elegiste la opcion 1')
-elif opcion == 2:
-    conversacion('Elegiste la opcion 2')   
-elif opcion == 3:
-    conversacion('Elegiste la opcion 3')   
-else:
-    print('Escribe la opcion correcta')
+# opcion = int(input('Elige una opcion (1, 2, 3): '))
+# if opcion == 1:
+#     conversacion('Elegiste la opcion 1')
+# elif opcion == 2:
+#     conversacion('Elegiste la opcion 2')   
+# elif opcion == 3:
+#     conversacion('Elegiste la opcion 3')   
+# else:
+#     print('Escribe la opcion correcta')
     
+def suma(a, b):
+    print('se suman dos numeros')
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
