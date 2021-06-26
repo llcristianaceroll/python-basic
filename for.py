8c4c7fb86ab0cf7165e76debd9d00c914c78dd43
Cristian <llcristianaceroll@gmail.com>
2021-06-26 08:35:25 -0500

    ciclo for

--- for.py
+++ for.py
@@ -7,6 +7,8 @@
 # a = list(range(1000))
 # print(a)
 
-for contador in range(1, 1001):
-    print(contador)
+# for contador in range(1, 1001):
+#     print(contador)
 
+for i in range(1, 11):
+    print(11 * i)
